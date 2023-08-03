969973d24a6be83fd655362dd0002da216d53cee
Romazan <zalalidinovroma@gmail.com>
2023-08-03 22:09:23 +0600

    adding doctor model

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    image = models.ImageField('Фото', upload_to='doctor_images/')
+    full_name = models.CharField('Полное имя', max_length=265)
+    age = models.IntegerField('Возраст')
+    job = models.CharField('Работа', max_length=265)
+    phone_number = models.CharField('Номер телефона', max_length=265)
+    description = models.TextField('Описание')
+
+    def __str__(self):
+        return f"{self.full_name}"
+    
+    class Meta: 
+        ordering = ('-id',)
+        verbose_name = 'Доктор'
+        verbose_name_plural = 'Доктора'
+
+
+        
