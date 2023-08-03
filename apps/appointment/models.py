65720cc41dbc92c277369f4efaaa91465a352f9f
Romazan <zalalidinovroma@gmail.com>
2023-08-03 22:40:12 +0600

    adding appointment model

--- apps/appointment/models.py
+++ apps/appointment/models.py
@@ -1,2 +1,23 @@
 from django.db import models
 
+class Appointment(models.Model):
+    GENDER_CHOICES = (
+        ('male', 'male'),
+        ('female', 'female'),    
+        )
+
+    full_name = models.CharField('Полное имя', max_length=265)
+    phone_number = models.CharField('Номер телефона', max_length=265)
+    sex = models.CharField('Пол', max_length=10, choices=GENDER_CHOICES)
+    time = models.TimeField('Время', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.full_name}-----{self.time}"
+    
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Встреча'
+        verbose_name_plural = 'Встречи'
+        
+
+
