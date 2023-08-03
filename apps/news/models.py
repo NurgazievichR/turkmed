044a538d86ed4291f09d82f322cb066cbfd22ed3
Romazan <zalalidinovroma@gmail.com>
2023-08-03 22:27:24 +0600

    add news model

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class New(models.Model):
+    title = models.CharField('Название', max_length=265)
+    description = models.TextField('Описание')
+    image = models.ImageField('Фото', upload_to='news_photos')
+    date = models.DateField('Дата', auto_now_add=True)
+    time = models.TimeField('Время', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+        
