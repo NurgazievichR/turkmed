5fef768ab63e66ce225b325899e275d94fe1a44b
Romazan <zalalidinovroma@gmail.com>
2023-08-03 22:47:21 +0600

    adding appointment urls

--- apps/appointment/admin.py
+++ apps/appointment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.appointment.models import Appointment
+
+admin.site.register(Appointment)
--- apps/appointment/views.py
+++ apps/appointment/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from apps.appointment.models import Appointment
+from apps.appointment.serializers import AppointmentSerializer
+
+class AppointmentAPIViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+    
