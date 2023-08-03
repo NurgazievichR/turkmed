7c707347c03629ebe0c3a3881bfb437d183e703e
Romazan <zalalidinovroma@gmail.com>
2023-08-03 23:21:22 +0600

    corsheaders

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ DJANGO_APPS = [
 THIRD_APPS = [
     'rest_framework',
     'drf_yasg',
+    "corsheaders",
 ]
 
 
@@ -60,6 +61,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -142,3 +144,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
