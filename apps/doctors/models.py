c1754b88f5ed9a125468dfbc6d9c8fa3920cd503
Romazan <zalalidinovroma@gmail.com>
2023-08-05 23:07:49 +0600

    award model

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -17,4 +17,17 @@ class Doctor(models.Model):
         verbose_name_plural = 'Доктора'
 
 
+class Award(models.Model):
+    title = models.CharField('Название', max_length=365)
+    image = models.ImageField('Фото', upload_to='awards/')
+
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Награда'
+        verbose_name_plural = 'Награды'
+
         
+    
