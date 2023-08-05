30034597757067826749ed017b728fdfba90b5f1
Romazan <zalalidinovroma@gmail.com>
2023-08-06 00:30:07 +0600

    add hospitalAward model

--- apps/doctors/admin.py
+++ apps/doctors/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
-from apps.doctors.models import Doctor
+from apps.doctors.models import Doctor, DoctorAward, HospitalAward
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
-admin.site.register(Doctor)
+class DoctorAwardInline(admin.TabularInline):
+    model = DoctorAward
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    inlines = (DoctorAwardInline,)
+
+admin.site.register(HospitalAward)
 
 
 
--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -26,6 +26,20 @@ class DoctorAward(models.Model):
         
     class Meta:
         ordering = ('-id',)
-        verbose_name = 'Фото'
-        verbose_name_plural = 'Фото'
+        verbose_name = 'Награда'
+        verbose_name_plural = 'Награды'
     
+
+class HospitalAward(models.Model):
+    title = models.CharField('Название', max_length=265)
+    image = models.ImageField('Фото', upload_to='hospital_awards_imeges/')
+
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Награда больницы'
+        verbose_name_plural = 'Награды больницы'
+
+
