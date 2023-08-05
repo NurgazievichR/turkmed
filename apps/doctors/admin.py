490f666bd744c0ad17767fbf4d629389f3e05c9a
Romazan <zalalidinovroma@gmail.com>
2023-08-05 23:12:44 +0600

    award admin add

--- apps/doctors/admin.py
+++ apps/doctors/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
-from apps.doctors.models import Doctor
+from apps.doctors.models import Doctor, Award
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
 admin.site.register(Doctor)
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_image')
+
+    def display_image(self, obj):
+        return obj.image.url if obj.image else None
+    display_image.short_description = 'Image'
+
+    
