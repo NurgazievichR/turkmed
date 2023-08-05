e4e1f3ea9131cbed2b87642e3f169b56df6a1e5c
Romazan <zalalidinovroma@gmail.com>
2023-08-05 23:54:01 +0600

    add DoctorAward model

--- apps/doctors/admin.py
+++ apps/doctors/admin.py
@@ -1,23 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from django.utils.safestring import mark_safe
 
-from apps.doctors.models import Doctor, Award
+from apps.doctors.models import Doctor
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
 admin.site.register(Doctor)
 
-@admin.register(Award)
-class AwardAdmin(admin.ModelAdmin):
-    list_display = ('display_image','title')
-
-    def display_image(self, obj):
-        if obj.image:
-            return mark_safe(f'<img src="{obj.image.url}" width="400" height="500" />')
-        return "No Image"
-    display_image.short_description = 'Image'
 
 
     
--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -17,17 +17,15 @@ class Doctor(models.Model):
         verbose_name_plural = 'Доктора'
 
 
-class Award(models.Model):
-    title = models.CharField('Название', max_length=365)
-    image = models.ImageField('Фото', upload_to='awards/')
+class DoctorAward(models.Model):
+    doctor = models.ForeignKey(Doctor, related_name='doctor_awards', on_delete=models.CASCADE, verbose_name='Доктор')
+    image = models.ImageField('Фото',upload_to='doctor_awards/')
 
     def __str__(self):
-        return f'{self.title}'
-    
+        return f"{self.doctor.full_name}--------{self.id}"
+        
     class Meta:
         ordering = ('-id',)
-        verbose_name = 'Награда'
-        verbose_name_plural = 'Награды'
-
-        
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
     
